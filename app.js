ba754cbb183fed0067842e0b2cded9fd10ceffa7
Patrik Holba <Paveegamedev@gmail.com>
2023-12-14 21:56:07 +0100

    word/todayWord now sends back the body if the JSON is correct

--- app.js
+++ app.js
@@ -5,13 +5,31 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const Ajv = require("ajv").default;
 const app = (0, express_1.default)();
 const port = 5000;
 // Apply CORS middleware
 app.use((0, cors_1.default)());
-// Your routes and other middleware go here
-app.get('/', (req, res) => {
-    res.send('Hello from the Node.js server!');
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+let wordRequestSchema = {
+    "type": "object",
+    "properties": {
+        "id": { "type": "number" },
+    },
+    "required": ["id"]
+};
+app.post('/word/todayWord', (req, res) => {
+    const { body } = req;
+    const ajv = new Ajv();
+    const valid = ajv.validate(wordRequestSchema, body);
+    if (!valid) {
+        res.status(400).send(ajv.errors);
+    }
+    else {
+        res.json(body);
+    }
 });
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
--- app.ts
+++ app.ts
@@ -1,16 +1,37 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from "body-parser";
+
+const Ajv = require("ajv").default;
 
 const app = express();
 const port = 5000;
 
 // Apply CORS middleware
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Your routes and other middleware go here
+let wordRequestSchema = {
+    "type": "object",
+    "properties": {
+        "id": { "type": "number"},
+    },
+    "required": ["id"]
+};
 
-app.get('/', (req, res) => {
-    res.send('Hello from the Node.js server!');
+app.post('/word/todayWord', (req, res) => {
+    const {body} = req
+    const ajv = new Ajv();
+    const valid = ajv.validate(wordRequestSchema, body);
+
+    if (!valid) {
+        res.status(400).send(ajv.errors);
+
+    }else{
+        res.json(body)
+    }
+
 });
 
 app.listen(port, () => {
